d74a1f8f43707dbc09afb001787b36fa0b2928b5
Mitch Crowe <crowe.mitch@gmail.com>
2017-04-18 19:42:48 -0700

    Release v0.0.1

---
+++ lib/cookie.d.ts
@@ -0,0 +1,7 @@
+declare var _default: {
+    serialize: (object: any) => string;
+    parse: (string: string) => {};
+    assign: (string: any, key: any, value: any) => string;
+    parseSetString: (string: string) => string;
+};
+export default _default;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,18 @@
-declare function doSomething(a: number): number;
-export default doSomething;
+export interface Map {
+    [key: string]: string;
+}
+declare class HeadlessBrowser {
+    headers: Map;
+    constructor(headers?: Map);
+    get(opts: any): Promise<any>;
+    setHeaders(headers: Map): void;
+    setHeader(key: string, value: string): void;
+    setCookie(key: string, value: string): void;
+    getCookies(): Map;
+    setCookies(cookies: Map | string): any;
+    getUserAgent(): string;
+    setUserAgent(value: string): void;
+    setAcceptEncoding(value: string): void;
+    setCookiesFromResponse(res: any): void;
+}
+export default HeadlessBrowser;
